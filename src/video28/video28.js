f7d0c0b5ff62d6a78932e6d8461b3a3357967917
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-05-21 18:37:15 +0700

    for/while/do-while

---
+++ src/video28/video28.js
@@ -0,0 +1,17 @@
+//for
+console.log("Video 28");
+for (let i = 0; i <= 5; i++) {
+  console.log(i);
+}
+//while
+let score = 10;
+while (score > 5) {
+  console.log(score);
+  score--;
+}
+//do while
+let count = 0;
+do {
+  console.log(count);
+  count++;
+} while (count < 5);
