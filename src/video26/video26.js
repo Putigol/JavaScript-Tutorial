19288dd94f71bdc39f564e305c15bba203735edb
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-05-21 17:32:14 +0700

    if-else

---
+++ src/video26/video26.js
@@ -0,0 +1,15 @@
+console.log("Video 26");
+// const age = 25;
+// if (age > 18) {
+//   console.log("Enjoy your video game");
+// } else {
+//   console.log("You are not old enough to play this video game");
+// }
+const score = 10;
+if (score > 9) {
+  console.log("Giỏi");
+} else if (score > 6 && score <= 9) {
+  console.log("Khá");
+} else {
+  console.log("Yếu");
+}
