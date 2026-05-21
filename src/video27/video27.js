2c09fb9f26b8030d660d5ed343d1969f24739020
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-05-21 17:45:44 +0700

    switch-case

---
+++ src/video27/video27.js
@@ -0,0 +1,17 @@
+console.log("Video 27");
+//score: giỏi, khá, trung bình, yếu
+const score = 7;
+switch (score) {
+  case (8, 9, 10):
+    console.log("Giỏi");
+    break;
+  case (6, 7):
+    console.log("Khá");
+    break;
+  case 5:
+    console.log("Trung bình");
+    break;
+  default:
+    console.log("Yếu");
+    break;
+}
