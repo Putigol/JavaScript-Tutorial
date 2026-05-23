4204056b5354b0803056dc171910416a00313a5f
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-05-23 23:09:58 +0700

    introduction to array

---
+++ src/video37/video37.js
@@ -0,0 +1,5 @@
+console.log("Video 37");
+const names = ["Alice", "Bob", "Charlie"];
+//Mảng có thể lưu trữ nhiều giá trị khác nhau,
+//bao gồm cả số, chuỗi, và thậm chí là các mảng khác.
+console.log(names);
