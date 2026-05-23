1c38d867594367bc68e345f0421dbb9c95d3cde1
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-05-23 11:24:21 +0700

    function

---
+++ src/video30/video30.js
@@ -0,0 +1,12 @@
+//Hãy khai báo hàm trước rồi gọi hàm
+console.log("Video 30");
+function greeting() {
+  console.log("Hello World");
+}
+greeting();
+//Tổng của a+b
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(10, 5));
+console.log(sum("Tôi học ", "lập trình:))"));
