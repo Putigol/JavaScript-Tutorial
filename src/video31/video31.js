2e218cdfc60f9e5de22ec4f1b6c5ce5f74125c57
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-05-23 14:38:41 +0700

    arrow function

---
+++ src/video31/video31.js
@@ -0,0 +1,16 @@
+console.log("Video 30");
+function sum(a, b) {
+  return a + b;
+}
+
+// (a, b) => {
+//   return a + b;
+// };
+// (function () {
+//   console.log("Hello from IIFE");
+// })();
+const tong = (a, b) => {
+  return a + b;
+};
+console.log(sum(5, 10));
+console.log(tong(5, 10));
