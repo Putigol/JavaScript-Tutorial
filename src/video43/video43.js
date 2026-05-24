288b749375c3b519a877f8d5eb9b9545dd2fe59c
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-05-24 15:07:02 +0700

    get/set

---
+++ src/video43/video43.js
@@ -0,0 +1,15 @@
+console.log("Video 43");
+const person = {
+  name: "John",
+  age: 30,
+};
+console.log(person);
+//get data
+console.log(person.name);
+//set data
+person["language"] = "English";
+person.address = "123 Main St";
+console.log(person);
+//delete data
+delete person.age;
+console.log(person);
