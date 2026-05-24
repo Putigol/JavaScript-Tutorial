fcc615264cc7f7bfcda7c9281c26833658e66e7d
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-05-24 14:49:02 +0700

    introduction to object

---
+++ src/video42/video42.js
@@ -0,0 +1,23 @@
+console.log("Video 42");
+const scores = [10, 4, 8, 6];
+//key: value
+const sv1 = {
+  name: "John",
+  score: 10,
+  address: {
+    city: "New York",
+    country: "USA",
+  },
+};
+
+const sv2 = {
+  name: "Jane",
+  score: 8,
+  address: {
+    city: "New York",
+    country: "USA",
+  },
+};
+
+const sinhvien = [sv1, sv2];
+console.log(sinhvien);
