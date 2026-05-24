9bc4ed41a7bd6564fed7253b8e4ceb74f15e26e1
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-05-24 10:31:56 +0700

    access/edit/add/erase array's elements

---
+++ src/video38/video38.js
@@ -0,0 +1,15 @@
+console.log("Video 38");
+const names = ["Alice", "Bob", "Charlie"];
+console.log(names[0]); // Alice
+console.log(names[1]); // Bob
+console.log(names[2]); // Charlie
+names[1] = "David";
+console.log(names[1]); // David
+names.push("Eve");
+console.log(names);
+names.unshift("Zara");
+console.log(names);
+names.pop();
+console.log(names);
+names.shift();
+console.log(names);
