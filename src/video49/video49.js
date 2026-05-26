a9f2e4cf88ab7aad72e423fb9f927cba4ce11c1e
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-05-26 12:05:54 +0700

    access HTML's elements by DOM

---
+++ src/video49/video49.js
@@ -0,0 +1,5 @@
+console.log("Video 49");
+const element = document.getElementById("username");
+const myDiv = document.querySelector(".xinchaouser");
+const allmyDiv = document.querySelectorAll(".xinchaouser");
+console.log(allmyDiv);
