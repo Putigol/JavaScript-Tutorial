149de39273a38a2dac48e0091eae8c3e29cf9073
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-02 11:25:58 +0700

    alert

---
+++ src/video54/video54.js
@@ -0,0 +1,7 @@
+console.log("Video 54");
+const loginButton = document.getElementById("login");
+console.log(loginButton);
+loginButton.addEventListener("click", () => {
+  console.log("Login button clicked");
+  alert("Welcome to the website!");
+});
