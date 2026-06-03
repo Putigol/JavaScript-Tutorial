08f9a68ef0f409bf4f2f8629192ae60d0a4052cf
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-03 10:06:25 +0700

    async-await

---
+++ src/video66/video66.js
@@ -0,0 +1,13 @@
+console.log("Video 66");
+//promise
+fetch("http://localhost:8000/users")
+  .then((res) => res.json())
+  .then((data) => console.log("fetch promise: ", data));
+
+//async await
+const fetchData = async () => {
+  const res = await fetch("http://localhost:8000/users");
+  const data = await res.json();
+  console.log(data);
+};
+fetchData();
