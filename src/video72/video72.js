b4c92414c239f00deeca6c6d0577809d6c0f69bb
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-06-13 16:21:13 +0700

    Homework 02: Part 2 (Them blog vao backend)

--- src/video72/video72.js
+++ src/video72/video72.js
@@ -21,4 +21,53 @@ const fetchLogs = async () => {
     });
   }
 };
+// Hàm addNewRowToEnd thêm dòng mô phỏng tạo một blog
+const addNewRowToEnd = (blog) => {
+  const tableBody = document.querySelector("#blogs tbody");
+  // Tạo phần tử dòng mới
+  const newRow = document.createElement("tr");
+  // Gán HTML cho dòng
+  newRow.innerHTML = `
+<tr>
+          <td>${blog.id}</td>
+          <td>${blog.title}</td>
+          <td>${blog.author}</td>
+          <td>${blog.content}</td>
+          <td>
+          <button>Xoá</button>
+          </td>
+</tr>
+`;
+  // Thêm dòng vào cuối bảng
+  tableBody.appendChild(newRow);
+};
+// Hàm handleAddNewBlog thêm blog vào backend
+const handleAddNewBlog = () => {
+  const title = document.getElementById("title");
+  const author = document.getElementById("author");
+  const content = document.getElementById("content");
+  const saveBlogButton = document.getElementById("saveBlog");
+  saveBlogButton.addEventListener("click", async () => {
+    // Gọi API để tạo một blog mới
+    const rawResponse = await fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: title.value,
+        author: author.value,
+        content: content.value,
+      }),
+    });
+    const data = await rawResponse.json();
+    addNewRowToEnd(data);
+    console.log("Phản hồi API create: ", data);
+    // if (data) {
+    //   fetchLogs();
+    // }
+  });
+};
 fetchLogs();
+handleAddNewBlog();
